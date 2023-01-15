948472fc3c00612845e8cc53416d6c514fd1bd1f
durid-ah <ahmad.durid96@gmail.com>
2023-01-14 20:15:55 -0500

    switch test server to use pymssql

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,28 @@
 from flask import Flask
+import pymssql
+
 app = Flask(__name__)
+
+server = 'mssqldb'
+database = 'sentiment_app'
+username = 'sa'
+password = 'DevPassword1'
+
 
 @app.route('/')
 def hello_world():
    return 'Hello, Docker!'
 
+@app.route('/data')
+def get_data():
+   conn = pymssql.connect(server=server, user=username, password=password)
+   cursor = conn.cursor()# as_dict=True)
 
+   cursor.execute('SELECT @@version')
+   print('DURID...')
+   for row in cursor:
+      print(row)
+
+   conn.close()
+
+   return 'yay'
